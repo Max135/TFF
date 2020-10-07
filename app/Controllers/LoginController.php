3b394183dd010de816eb2716d08af666fbe3a6d7
Woontopia <xavloiselle@hotmail.com>
2020-10-07 10:32:07 -0400

    login start

--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -9,11 +9,23 @@ class LoginController extends \Zephyrus\Application\Controller
     public function initializeRoutes()
     {
         $this->get("/index", "index");
+        $this->get("/login", "login");
+        $this->post("/login", "loginUser");
     }
 
     public function index() {
         return $this->render("index", [
            'title'=>'Index'
         ]);
+    }
+
+    public function login() {
+        return $this->render("login", [
+            'title' => 'Login'
+        ]);
+    }
+
+    public function loginUser() {
+
     }
 }
