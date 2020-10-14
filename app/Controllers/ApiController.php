cb1bdbec34015e2079740c285a1d81b70c27280d
Max <maxlamenace_2000@hotmail.com>
2020-10-14 18:56:07 -0400

    api functions

--- app/Controllers/ApiController.php
+++ app/Controllers/ApiController.php
@@ -1,11 +1,21 @@
 <?php namespace Controllers;
 
-
 class ApiController extends Controller
 {
 
     public function initializeRoutes()
     {
-        $this->get("/api", "api");
+        $this->get("/api", "apiGet");
+        $this->post("/api", "apiPost");
+    }
+
+    public function apiPost()
+    {
+
+    }
+
+    public function apiGet()
+    {
+
     }
 }
