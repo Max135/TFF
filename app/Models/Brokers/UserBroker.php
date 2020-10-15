ea67a14647d833b56b035e9883e4c4c331390721
Max <maxlamenace_2000@hotmail.com>
2020-10-15 15:51:16 -0400

    User broker modifications

--- app/Models/Brokers/UserBroker.php
+++ app/Models/Brokers/UserBroker.php
@@ -1,18 +1,30 @@
 <?php namespace Models\Brokers;
 
+use Zephyrus\Security\Cryptography;
+
 class UserBroker extends Broker
 {
     public function insert(string $email, string $password, string $username): int
     {
         $sql = "insert into User values (default, ?, ?, ?, null);";
-        $this->query($sql, [$email, $password, $username]);
+        $this->query($sql, [$email, Cryptography::hashPassword($password), $username]);
 
         return $this->getDatabase()->getLastInsertedId();
     }
 
     public function findById(int $id)
     {
-        $sql = "select username, password from User where id = ?;";
+        $sql = "select * from User where id = ?;";
         return $this->selectSingle($sql, [$id]);
+    }
+
+    public function validCredentials(string $email, string $password): bool
+    {
+        $sql = "select password from User where email = ?";
+        $hashedPassword = $this->selectSingle($sql, [$email]);
+        if ($hashedPassword != null) {
+            return Cryptography::verifyHashedPassword($password, $hashedPassword);
+        }
+        return false;
     }
 }
