03dfc7f2b33c01fa6f56b767985f7d4493ec47b4
Max <maxlamenace_2000@hotmail.com>
2020-11-11 21:26:07 -0500

    Practically finished the whole project by myself with this commit

--- app/Models/Brokers/CatchBroker.php
+++ app/Models/Brokers/CatchBroker.php
@@ -2,9 +2,16 @@
 
 class CatchBroker extends Broker
 {
+    //Lat/lon still depends on a model of the earth. Typically, when people say lat/lon,
+    // the WGS84 reference geoid is assumed, but you can have lat/lon with a different geoid, hence the SRID 4326 that combines these two.
+
+    //SELECT X(coordinates),Y(coordinates) FROM table
+
+    //https://webmasters.stackexchange.com/questions/76607/browser-shows-ip-address-instead-of-my-domain-name-when-using-google-cloud
+
     public function insert($tripId, $temperature, $pressure, $humidity, $time, $lng, $lat, $hotspotId = null)
     {
-        $sql = "insert into Catch (id, tripId, hotspotId, temperature, barometricPressure, humidity, time, coordinates) values (default, ?, ?, ?, ?, ?, ?, point(?, ?));";
+        $sql = "insert into Catch values (default, ?, ?, ?, ?, ?, ?, point(?, ?));";
         $this->query($sql, [$tripId, $hotspotId, $temperature, $pressure, $humidity, $time, $lng, $lat]);
     }
 }
